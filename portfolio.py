127b169ba663ebffdb182a2cc11bdd13076a9e64
stutimishra9 <32788677+stutimishra9@users.noreply.github.com>
2021-11-15 20:11:13 -0500

    Portfolio class
    class added

--- portfolio.py
+++ portfolio.py
@@ -7,3 +7,11 @@ price, a float, the price paid per share
 and has a second method called cost which returns a float, the total cost paid for all stocks in the portfolio
 Consider that to implement the cost method, you'll need to be storing the purchases made each time the buy method is called somewhere. You may do so by any means convenient to you.
 Commit this file to your repository and push it to GitHub using GitHub Desktop, with a suitable commit message."""
+
+class Portfolio:
+    def __init__(self):
+        self._stocks=[]
+    def buy(self,name,shares,price):
+        self._stocks.append(name,shares,price)
+    def cost(self):
+        return sum(shares*price for _,shares,price in self._stocks)
